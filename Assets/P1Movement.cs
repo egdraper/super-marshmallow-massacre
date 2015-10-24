db75a299aa6b8a8f8a294f24e49be83c10c7da56
Rapscaliwag <dusty.hooligan@gmail.com>
2015-10-24 16:23:28 -0600

    P1Movement

--- Assets/P1Movement.cs
+++ Assets/P1Movement.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class P1Movement : MonoBehaviour {
+public class P1Movement : MonoBehaviour
+{
+    public float speed = 5;
+    public float gravity;
 
-	// Use this for initialization
-	void Start () {
-	//blah
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Left Stick X Axis");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+        GetComponent<Rigidbody>().velocity = movement * speed;
 	}
 }
