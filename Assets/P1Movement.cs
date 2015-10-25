61a9028145dec29748acc69563e1e7ac0e81cdd7
Rapscaliwag <dusty.hooligan@gmail.com>
2015-10-24 18:34:04 -0600

    Trying to figure out gravity...

--- Assets/P1Movement.cs
+++ Assets/P1Movement.cs
@@ -3,14 +3,49 @@ using System.Collections;
 
 public class P1Movement : MonoBehaviour
 {
-    public float speed = 5;
+    private Rigidbody player;
+    public float moveSpeed;
+    public float jumpSpeed;
+    private float fallSpeed;
     public float gravity;
+
+    void Start()
+    {
+        player = GetComponent<Rigidbody>();
+        fallSpeed = jumpSpeed * (-1);
+    }
 
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Left Stick X Axis");
+        CharacterController controller = GetComponent<CharacterController>();
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
-        GetComponent<Rigidbody>().velocity = movement * speed;
+        //Storing value from moving left joystick
+        float moveHorizontal = Input.GetAxis("Left Stick X Axis");
+        float moveVertical = player.velocity.y;
+
+        //Detecting whether or not jump button was pressed, if the player was touching the ground, and if a jump should take place
+        if ((Input.GetButtonDown("A Button")) && (controller.isGrounded))
+            moveVertical = jumpSpeed;
+        else if (controller.isGrounded)
+        {
+            moveVertical = 0.001f;
+            print("Grounded!");
+        }
+        else
+        {
+            moveVertical -= gravity; //crazy gravity equation
+            if (moveVertical < fallSpeed)
+                moveVertical = fallSpeed;
+        }
+
+        //Moving the player
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+        player.transform.Translate(movement*Time.deltaTime*moveSpeed);
 	}
+
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == "Floor")
+    //        touchFloor = true;
+    //}
 }
