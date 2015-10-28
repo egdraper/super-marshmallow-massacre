fd47e67987de29cf98e51f68c2c100427a77edd2
Rapscaliwag <dusty.hooligan@gmail.com>
2015-10-27 21:19:21 -0600

    Wall slide.  --DP

--- Assets/P1Movement.cs
+++ Assets/P1Movement.cs
@@ -10,7 +10,8 @@ public class P1Movement : MonoBehaviour
     public float gravity;
     private bool blockTouch;
     public float wallThrust;
-    public int wallJumpTime;
+    private int wallJumpTime;
+    public float wallJumpTimeDuration;
 
     void Start()
     {
@@ -24,7 +25,13 @@ public class P1Movement : MonoBehaviour
         CharacterController controller = GetComponent<CharacterController>();
         Rigidbody rBody = GetComponent<Rigidbody>();
 
-        movement.x = Input.GetAxis("Left Stick X Axis") * moveSpeed;
+        if ((wallJumpTime == 0) || (wallJumpTime > wallJumpTimeDuration))
+        {
+            movement.x = Input.GetAxis("Left Stick X Axis") * moveSpeed;
+            wallJumpTime = 0;
+        }
+        else
+            wallJumpTime++;
 
         //Jump
         if (controller.isGrounded)
@@ -34,7 +41,7 @@ public class P1Movement : MonoBehaviour
                 movement.y = jumpSpeed;
         }
 
-        //Wall Jump
+        //Wall Jump and Wall Slide
         if ((blockTouch == true) && (controller.isGrounded == false))
         {
             if (Input.GetButton("A Button"))
@@ -42,20 +49,33 @@ public class P1Movement : MonoBehaviour
                 if (movement.x > 0) //Player is going right
                 {
                     movement.y = jumpSpeed;
+                    movement.x = moveSpeed * (-1);
+                    wallJumpTime = 1;
                 }
                 else if (movement.x < 0) //Player is going left
                 {
                     movement.y = jumpSpeed;
+                    movement.x = moveSpeed;
+                    wallJumpTime = 1;
                 }
             }
+            if ((movement.x != 0) && (controller.velocity.x == 0))
+                fallSpeed = (jumpSpeed * (-1)) / 4; //Wall slide speed equation
+            else
+                fallSpeed = jumpSpeed * (-1);
         }
+        else
+            fallSpeed = jumpSpeed * (-1);
 
         //Movement
         if ((controller.velocity.y == 0) && (controller.isGrounded == false))
             movement.y = 0;
+
         movement.y -= gravity * Time.deltaTime;
+
         if (movement.y < fallSpeed)
             movement.y = fallSpeed;
+
         controller.Move(movement * Time.deltaTime);
 	}
 
